2e56f86ea9f52e10f795a89ad2cbcdf8021b5a6f
janothan <jan.portisch@gmail.com>
2020-10-01 19:21:34 +0200

    setup.py added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="kbc_rdf2vec",
+    version="0.1",
+    packages=["kbc_rdf2vec"],
+    url="www.jan-portisch.eu",
+    license="MIT",
+    author="Jan Portisch",
+    author_email="jan@informatik.uni-mannheim.de",
+    description="Allows to use RDF2Vec for knowledge base completion (KBC) / link predictions.",
+)
