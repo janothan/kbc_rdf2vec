880fa9fecae477557bb50908d4f4953b393f14bd
janothan <jan.portisch@gmail.com>
2021-01-13 09:15:37 +0100

    Log fix for distributable module.

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
     author="Jan Portisch",
     author_email="jan@informatik.uni-mannheim.de",
     description="Allows to use RDF2Vec for knowledge base completion (KBC) / link predictions.",
-    package_data={"kbc_rdf2vec": ["datasets/fb15k/*", "datasets/wn18/*"]},
+    package_data={"kbc_rdf2vec": ["log.conf", "datasets/fb15k/*", "datasets/wn18/*"]},
 )
