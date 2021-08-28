861f26fe324f58006828b70d407dddfefccdf243
janothan <jan.portisch@gmail.com>
2021-08-28 15:33:17 +0200

    unit test

--- tests/test_rdf2vec_kbc.py
+++ tests/test_rdf2vec_kbc.py
@@ -2,6 +2,7 @@ import os
 
 from kbc_rdf2vec.dataset import DataSet
 from kbc_rdf2vec.rdf2vec_kbc import Rdf2vecKbc
+from kbc_rdf2vec.prediction import PredictionFunctionEnum
 
 
 class TestRdf2vecKbc:
@@ -90,6 +91,26 @@ class TestRdf2vecKbc:
             file_for_predicate_exclusion=nt_file_path,
         )
         file_to_write = "./test_prediction.txt"
+        kbc.predict(file_to_write=file_to_write)
+        assert os.path.isfile(file_to_write)
+        with open(file_to_write, "r", encoding="utf8") as f:
+            content = f.read()
+            assert content.count("_member_meronym") == 0
+        os.remove(file_to_write)
+
+    def test_prediction_function_ann(self):
+        """Tests whether relations are excluded from proposals and whether ANN works."""
+        model_file_path = "./tests/test_resources/wn_test_model.kv"
+        nt_file_path = "./tests/test_resources/wn_test.nt"
+        if not os.path.isfile(model_file_path):
+            os.chdir("./..")
+        kbc = Rdf2vecKbc(
+            data_set=DataSet.WN18,
+            model_path=model_file_path,
+            file_for_predicate_exclusion=nt_file_path,
+            prediction_function=PredictionFunctionEnum.ANN,
+        )
+        file_to_write = "./test_ann_prediction.txt"
         kbc.predict(file_to_write=file_to_write)
         assert os.path.isfile(file_to_write)
         with open(file_to_write, "r", encoding="utf8") as f:
